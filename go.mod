c6c26221d38a3eb3b7cfb4962b57b7a77e0efa7d
Utkan Sahin <utkan.sahin@anapaya.net>
2025-05-15 12:10:26 +0200

    add zebra file

--- go.mod
+++ go.mod
@@ -6,11 +6,13 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/vishvananda/netlink v1.3.0
 	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba
+	github.com/osrg/gobgp/v3 v3.37.0
 )
 
 require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
-	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
---
+++ zebra.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/netip"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/osrg/gobgp/v3/pkg/log"
+	"github.com/osrg/gobgp/v3/pkg/zebra"
+)
+
+const (
+	reconnectInterval = time.Second
+	zebraAPIVersion   = 6 // we only support FRR 8.2, which uses Zebra version 6
+)
+
+// Zebra manages route exchanges with the Zebra daemon in the FRR suite.
+type Zebra struct {
+	// ZebraServer the path to the zebra server unix socket.
+	// It can be made to connect via TCP by using the following format: tcp://<host>:<port>
+	ZebraServer string
+	// ClientType is the type of the client. All published will be of this type.
+	// Note that routes published by client of one type are never redistributed to a client
+	// of the same type.
+	ClientType zebra.RouteType
+
+	mtx sync.Mutex
+	// closeChan is used to shut down the FRR connection.
+	closeChan chan struct{}
+	// zebraSoftware is zebra software (quagga, frr, cumulus).
+	zebraSoftware zebra.Software
+}
+
+func (z *Zebra) init() {
+	z.mtx.Lock()
+	defer z.mtx.Unlock()
+	if z.closeChan == nil {
+		z.zebraSoftware = zebra.NewSoftware(zebraAPIVersion, "frr8.2")
+		z.closeChan = make(chan struct{})
+	}
+}
+
+func (z *Zebra) Close() {
+	z.init()
+	close(z.closeChan)
+}
+
+func (z *Zebra) connect() *zebra.Client {
+	for {
+		network := "unix"
+		address := z.ZebraServer
+		if strings.HasPrefix(z.ZebraServer, "tcp://") {
+			network = "tcp"
+			address = address[6:]
+		}
+		client, err := zebra.NewClient(log.NewDefaultLogger(), network, address, z.ClientType,
+			zebraAPIVersion, z.zebraSoftware, 0)
+		if err == nil {
+			fmt.Println("Connected to FRR", "zserv", z.ZebraServer)
+			return client
+
+		}
+		fmt.Println("Could not connect to FRR", "zserv", z.ZebraServer, "err", err)
+		wait := time.After(reconnectInterval)
+		select {
+		case <-wait:
+			// Retry to connect to FRR.
+			continue
+		case <-z.closeChan:
+			// Closed by the user.
+			return nil
+		}
+	}
+}
+
+func (z *Zebra) addRoute(client *zebra.Client, prefix netip.Prefix, nextHop netip.Addr) error {
+	routeBody := &zebra.IPRouteBody{
+		Type: z.ClientType,
+		Safi: zebra.SafiUnicast,
+		Prefix: zebra.Prefix{
+			Prefix:    prefix.Addr().AsSlice(),
+			PrefixLen: uint8(prefix.Bits()),
+		},
+		Message:  zebra.MessageNexthop,
+		Nexthops: []zebra.Nexthop{{Gate: nextHop.AsSlice()}},
+	}
+	return client.SendIPRoute(0, routeBody, false)
+}
+
+func (z *Zebra) delRoute(client *zebra.Client, prefix netip.Prefix) error {
+	routeBody := &zebra.IPRouteBody{
+		Type: z.ClientType,
+		Safi: zebra.SafiUnicast,
+		Prefix: zebra.Prefix{
+			Prefix:    prefix.Addr().AsSlice(),
+			PrefixLen: uint8(prefix.Bits()),
+		},
+	}
+	return client.SendIPRoute(0, routeBody, true)
+}
